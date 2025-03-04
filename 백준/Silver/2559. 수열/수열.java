6a01543bb9a5ebef0b7b753d52a93255f7d58770
zlnongi <108576828+zlnongi@users.noreply.github.com>
2025-03-05 03:16:36 +0900

    [Silver III] Title: 수열, Time: 244 ms, Memory: 23780 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,33 +1,32 @@
 import java.util.*;
 import java.io.*;
 
+// 누적합
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] ondo = new int[n+1];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] ondo = new int[N+1];
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        for (int i=1; i<=N; i++) {
+            ondo[i] = ondo[i-1] + Integer.parseInt(st1.nextToken());
+        }  // 입력
 
-        StringTokenizer st1= new StringTokenizer(br.readLine());
-        for (int i=1; i<=n; i++) {
-            ondo[i] = Integer.parseInt(st1.nextToken());
-        }
+        int sum = 0;
         int max = Integer.MIN_VALUE;
 
-        for (int i=1; i<=n-k+1; i++) {
-            int sum = 0;
-
-            for (int j = i; j <= i+k-1; j++) {
-                sum += ondo[j];
-            }
-
-            if (max < sum) max = sum;
-
+        for (int i=1; i<=N-K+1; i++) {
+            int start = i;
+            int end = i+K-1;
+            sum = ondo[end] - ondo[start-1];
+            max = Math.max(max, sum);
         }
         System.out.println(max);
 
-
     }
+
+
 }
