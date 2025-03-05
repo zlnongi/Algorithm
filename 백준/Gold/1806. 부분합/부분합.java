3aceceee7d532f2fa89ac7d97295dece594512b4
zlnongi <108576828+zlnongi@users.noreply.github.com>
2025-03-06 02:22:54 +0900

    [Gold IV] Title: 부분합, Time: 244 ms, Memory: 23904 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,36 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N+1];
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n+1];
 
         StringTokenizer st1 = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
+        for (int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st1.nextToken());
         }
 
-        int min = Integer.MAX_VALUE;
         int start = 0;
         int end = 0;
         int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int cnt = 0;
 
-        while (start <= N && end <= N) {
-            if (sum >= S) {
+        while (start<=n && end<=n) {
+
+            if (sum < s) {
+                sum += arr[end];
+                end++;
+
+            } else if (sum >= s) {
                 sum -= arr[start];
                 min = Math.min(min, end-start);
                 start++;
-            } else if (sum < S) {
-                sum += arr[end];
-                end++;
             }
         }
-        if (min == Integer.MAX_VALUE) System.out.println(0);
+
+        if (min == Integer.MAX_VALUE) System.out.println("0");
         else System.out.println(min);
     }
 }
