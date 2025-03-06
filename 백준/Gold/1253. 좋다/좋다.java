45aae1e4ed48a482bb1bed7f57693b2ed40f28b1
zlnongi <108576828+zlnongi@users.noreply.github.com>
2025-03-06 21:58:37 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -9,33 +9,42 @@ public class Main {
         int[] arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for (int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-        }
+        }  // 입력
 
-        Arrays.sort(arr); // 정렬 해줘야됨 !!! 1 2 3 4 이런식으로 안 나올 수도 있으니까
+        Arrays.sort(arr);
+
         int cnt = 0;
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<n; i++) {  // 0-n까지
             int start = 0;
             int end = n-1;
 
-            while (true) {  // 무한 루프 생성 후 break 통해 빠져나오는 패턴
-                // 자기 자신을 더하는 건 안 되므로 이동시키는 것 !!
-                if (start == i) start++;
-                if (end == i) end--;
-                if (start >= end) break;
+            while(start < end) { // 진짜????>........ 조건(이분탐색이거나 투포인트의 경우 start<end)을 걸기..............
+                if (start == i) {
+                    start++;
+                    continue;
+                } else if (end == i){
+                    end--;
+                    continue;
+                }
 
-                if (arr[start] + arr[end] < arr[i]) start++;
-                else if (arr[start] + arr[end] > arr[i]) end--;
-                else if (arr[start] + arr[end] == arr[i]) {
+                int sum = arr[start] + arr[end];
+
+                if (sum == arr[i]) {  // 내가 원하는 조건
                     cnt++;
                     break;
+                } else if (sum > arr[i]) {
+                    end--;
+                } else {
+                    start++;
                 }
+
             }
 
         }
         System.out.println(cnt);
+
 
     }
 
