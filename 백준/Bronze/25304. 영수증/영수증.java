47baa2168ab73f31b8e0cc7e842e89a0b68eb90d
zlnongi <108576828+zlnongi@users.noreply.github.com>
2025-05-06 01:18:28 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sum += (a*b);
+        }
+
+        if (sum == x) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+
+
+    }
+
+}
